e686d2c5a29fad14b38370ff07729cf73595d08a
Jeff Cadieux <jeffcadieux@hotmail.com>
2021-02-25 15:20:14 +0900

    Separate CRA app to client, add express backend

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
 
 function StripeCheckoutButton({ price }) {
   const priceForStripe = price * 100
   const publishableKey = 'pk_test_51INDsBJ4gSS7mH8o2i1k1yHuaaCfEvK0fGli2kOciLQwTlYzTjt2BycyS25maihCR9lTueUBTGQFNms50BzhkvaK00N7b24U2H'
 
   const onToken = token => {
-    console.log(token)
-    alert('Payment succeeded!')
+    axios({
+      url: 'payment',
+      method: 'POST',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => alert('Payment succeeded!'))
+      .catch(error => {
+        console.log('Payment error: ', JSON.parse(error))
+        alert('There was a problem with payment. Please use the provided credit card.')
+      })
   }
 
   return (
